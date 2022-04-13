64ae03f02f6dbd63a1c12089d0aad8f50a27486c
GenaroLaureano <laureanomg2@gmail.com>
2022-04-12 19:12:57 -0500

    api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +19,38 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/users', function (Request $request) {
+    return User::all();
+});
+
+Route::post('/users', function (Request $request) {
+    return User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => $request->password,
+    ]);
+});
+
+Route::put('/users/{user}', function (User $user, Request $request) {
+   
+    $success = $user->update([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => $request->password,
+    ]);
+
+    return [
+        'success' => $success,
+    ];
+});
+
+Route::delete('/users/{user}', function (User $user) {
+   
+    $success = $user->delete();
+
+    return [
+        'success' => $success,
+    ];
+});
+
