bc9a7efd33d90281d98f0639b61dc0436dd2b63c
Genaro <laureanomg2@gmail.com>
2022-04-13 17:43:10 -0500

    template

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('state')->nullable();
+            $table->string('municipality')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('profilePicture')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -20,29 +21,48 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/users', function (Request $request) {
+Route::get('/users', function () {
     return User::all();
 });
 
 Route::post('/users', function (Request $request) {
-    return User::create([
+    $user = User::create([
         'name' => $request->name,
         'email' => $request->email,
         'password' => $request->password,
+        'state' => $request->state,
+        'municipality' => $request->municipality,
+        'phone' => $request->phone,
     ]);
+    
+    if($request->profilePicture) {
+        $path = $request->profilePicture->store('public/profilePictures');
+        $user->profilePicture = $path;
+        $user->save();
+    }
+
+    return response()->json($user, 201);
 });
 
 Route::put('/users/{user}', function (User $user, Request $request) {
    
-    $success = $user->update([
+    $user->update([
         'name' => $request->name,
         'email' => $request->email,
         'password' => $request->password,
+        'state' => $request->state,
+        'municipality' => $request->municipality,
+        'phone' => $request->phone,
     ]);
 
-    return [
-        'success' => $success,
-    ];
+    if($request->profilePicture) {
+        $path = $request->profilePicture->store('public/profilePictures');
+        $user->profilePicture = $path;
+        $user->save();
+    }
+
+    return response()->json($user, 200);
+
 });
 
 Route::delete('/users/{user}', function (User $user) {
@@ -54,3 +74,184 @@ Route::delete('/users/{user}', function (User $user) {
     ];
 });
 
+Route::get('/users/{user}', function (User $user) {
+    return $user;
+});
+
+
+Route::get('/templates', function () {
+    $postModel = new Post();
+    $posts = Post::all();
+    $template = [];
+    foreach($posts as $post){
+        $VerticalArrangements = [       
+            'id' => 'VerticalArrangement' . $postModel->cadenaAleatoria($post->id),
+            'type' => 'VerticalArrangement',
+            'properties' => [
+                'AlignHorizontal' => 3,
+                'BackgroundColor' => -8689426,
+                'Width' => -2
+            ],
+            'components' => [
+                [
+                    'id' => 'Label' .  $postModel->cadenaAleatoria($post->id),
+                    'type' => 'Label',
+                    'properties' => [
+                        'FontSize' => 2
+                    ]
+                ],
+                [
+                    'id' => 'HorizontalArrangement' .  $postModel->cadenaAleatoria($post->id),
+                    'type' => 'HorizontalArrangement',
+                    'properties' => [
+                        'BackgroundColor' => -1,
+                        'Width' => -1098
+                    ],
+                    'components' => [
+                        [
+                            'id' => 'Image' . $post->id,
+                            'type' => 'Image',
+                            'properties' => [
+                                'Height' => -2,
+                                'Width' => -1020,
+                                'Picture' => 'download.png'
+                            ]
+                        ],
+                        [
+                            'id' => 'VerticalArrangement'.  $postModel->cadenaAleatoria($post->id),
+                            'type' => 'VerticalArrangement',
+                            'properties' => [
+                                'AlignHorizontal' => 2,
+                                'Width' => -2
+                            ],
+                            'components' => [
+                                [
+                                    'id' => 'Label' .  $postModel->cadenaAleatoria($post->id),
+                                    'type' => 'Label',
+                                    'properties' => [
+                                        'FontBold' => true,
+                                        'FontSize' => 20,
+                                        'Width' => -2,
+                                        'Text' => $post->text
+                                    ]
+                                ],
+                                [
+                                    'id' => 'HorizontalArrangement' .  $postModel->cadenaAleatoria($post->id),
+                                    'type' => 'HorizontalArrangement',
+                                    'properties' => [
+                                        'AlignHorizontal' => 3,
+                                        'AlignVertical' => 2
+                                    ],
+                                    'components' => [
+                                        [
+                                            'id' => 'ButtonUnlike' . $post->id,
+                                            'type' => 'Button',
+                                            'properties' => [
+                                                'FontSize' => 16,
+                                                'Height' => -2,
+                                                'Width' => -2,
+                                                'Image' => 'fake.png'
+                                            ]
+                                        ],
+                                        [
+                                            'id' => 'Label' .  $postModel->cadenaAleatoria($post->id),
+                                            'type' => 'Label',
+                                            'properties' => [
+                                                'FontSize' => 16,
+                                                'Text' => '3',
+                                                'TextAlignment' => 1
+                                            ]
+                                        ],
+                                        [
+                                            'id' => 'Label' .  $postModel->cadenaAleatoria($post->id),
+                                            'type' => 'Label',
+                                            'properties' => [
+                                                'Text' => '   '
+                                            ]
+                                        ],
+                                        [
+                                            'id' => 'ButtonLike' . $post->id . $post->id,
+                                            'type' => 'Button',
+                                            'properties' => [
+                                                'Height' => -2,
+                                                'Width' => -2,
+                                                'Image' => 'true.png'
+                                            ]
+                                        ],
+                                        [
+                                            'id' => 'Label' .  $postModel->cadenaAleatoria($post->id),
+                                            'type' => 'Label',
+                                            'properties' => [
+                                                'FontSize' => 16,
+                                                'Text' => '4',
+                                                'TextAlignment' => 1
+                                            ]
+                                        ],
+                                        [
+                                            'id' => 'Label' .  $postModel->cadenaAleatoria($post->id),
+                                            'type' => 'Label',
+                                            'properties' => [
+                                                'Text' => '  '
+                                            ]
+                                        ]
+                                    ]
+                                ],
+                                [
+                                    'id' => 'Label' . $postModel->cadenaAleatoria($post->id),
+                                    'type' => 'Label',
+                                    'properties' => [
+                                        'FontSize' => 2
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ],
+                [
+                    'id' => 'Label' . $postModel->cadenaAleatoria($post->id),
+                    'type' => 'Label',
+                    'properties' => [
+                        'FontSize' => 2
+                    ]
+                ]
+            ]
+        ];
+        
+        array_push($template, $VerticalArrangements);
+    }
+
+
+    $screen = [
+        'name' => 'blogs',
+        'metadata-version' => 1,
+        'extension_version' => 5,
+        'author' => 'Genaro',
+        'platforms' => [
+            'ai2.appinventor.mit.edu'
+        ],
+        'extensions' => [],
+        'keys' => [],
+        'components' => $template
+    ];
+
+    return response()->json($screen, 200);
+});
+
+
+
+Route::post('/posts', function (Request $request) {
+    $post = Post::create([
+        'text' => $request->text,
+        'likes' => $request->likes,
+        'dislikes' => $request->dislikes,
+        'user_id' => $request->user_id,
+    ]);
+    
+    if($request->postImage) {
+        $path = $request->postImage->store('public/postImages');
+        $post->postImage = $path;
+        $post->save();
+    }
+
+    return response()->json($post, 201);
+});
